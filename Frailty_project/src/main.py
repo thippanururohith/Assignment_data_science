63baee51441a1ca7532db391fa6e500e2f81983c
thippanururohith <113183667+thippanururohith@users.noreply.github.com>
2023-02-21 04:23:45 +0000

    second commit

--- Frailty_project/src/main.py
+++ Frailty_project/src/main.py
@@ -1,5 +1,8 @@
-import pandas as pd;
-
-df = pd.readcsv("../data_raw/raw_fertility_data.csv")
-
+import pandas as pd
+import sys
+sys.path.insert(0, '.')
+df = pd.read_csv("Frailty_project/data_raw/raw_frailty_data.csv")
+print(df.columns)
+df=df[df['Frailty']=='Y']
+df.to_csv("Frailty_project/data_clean/Clean_Fraility_data.csv",index=False)
 print(df)
